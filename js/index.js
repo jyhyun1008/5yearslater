9a2f4e63b88711d1083ac1f4cae713ed39581275
Hyun, Jaeyeon <93899740+jyhyun1008@users.noreply.github.com>
2023-02-02 08:53:14 +0900

    Create index.js

---
+++ js/index.js
@@ -0,0 +1,59 @@
+let vh = window.innerHeight * 0.01;
+let vw = window.innerWidth * 0.01;
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+// 리사이즈
+window.addEventListener('resize', () => {
+    let vh = window.innerHeight * 0.01;
+    let vw = window.innerWidth * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+})
+
+function getToday(){
+    var date = new Date();
+    var year = date.getFullYear();
+    var month = ("0" + (1 + date.getMonth())).slice(-2);
+    var day = ("0" + date.getDate()).slice(-2);
+
+    return year + month + day;
+}
+
+function parseMd(md){
+
+    var md0 = md;
+
+    //images with links
+    md = md.replace(/\!\[([^\]]+)\]\(([^\)]+)\)[\(]{1}([^\)\"]+)(\"(.+)\")?[\)]{1}/g, '<div class="gallery"><a href="$3"><img src="$2" alt="$1" width="100%" /></a></div>');
+    
+    //images
+    md = md.replace(/\!\[([^\]]+)\]\(([^\)]+)\)/g, '<img src="$2" alt="$1" width="100%" />');
+    
+    //links
+    md = md.replace(/[\[]{1}([^\]]+)[\]]{1}[\(]{1}([^\)\"]+)(\"(.+)\")?[\)]{1}/g, '<a href="$2" title="$4">$1</a>');
+    
+    //code
+    md = md.replace(/[\`]{1}([^\`]+)[\`]{1}/g, '<code>$1</code>');
+
+    //br
+    md = md.replace(/\n\n/g, '</p><p>');
+    
+    return md;
+}
+
+const today = getToday();
+const diary = document.querySelector("#diary")
+
+if (vw >= 700) {
+
+var url = "https://raw.githubusercontent.com/jyhyun1008/5yearslater/main/diary/"+today+".md"
+    fetch(url)
+    .then(res => res.text())
+    .then((out) => {
+        document.querySelector("#diary").innerHTML += parseMd(out)
+    })
+    .catch(err => { throw err });
+
+} else {
+
+
+}
